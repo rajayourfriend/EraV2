deceac1cb3ccb2c589fdea7bac3fa188ce7e050e
Raja P <42088982+rajayourfriend@users.noreply.github.com>
2024-03-20 20:15:28 +0530

    added BN
    added BN

--- S8/model.py
+++ S8/model.py
@@ -11,9 +11,63 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+
+## For S8 of EraV2 - Batch Normalization
+
+class Net_BN_S8(nn.Module):
+    def __init__(self):
+        super(Net_BN_S8, self).__init__()
+        set1 = 16 #channels
+        set2 = 32 #channels
+        out = 10 #channels
+        avg = 5 #channels
+        drop = 0.25 #dropout
+        mom = 0.1
+        self.conv1 = nn.Conv2d(3, set1, 3, padding=1) #first 3 => input channels(R,G,B) last 3 => kernel size (3x3)
+        self.bn1 = nn.BatchNorm2d(num_features=set1, eps=1e-05, momentum=mom, affine=True, track_running_stats=True)
+        self.conv2 = nn.Conv2d(set1, set1, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(num_features=set1, eps=1e-05, momentum=mom, affine=True, track_running_stats=True)
+        self.conv3 = nn.Conv2d(set1, set1, 1, padding=1)
+        self.bn3 = nn.BatchNorm2d(num_features=set1, eps=1e-05, momentum=mom, affine=True, track_running_stats=True)
+        self.pool1 = nn.MaxPool2d(2, 2)
+
+        self.conv4 = nn.Conv2d(set1, set1, 3, padding=1)
+        self.bn4 = nn.BatchNorm2d(num_features=set1, eps=1e-05, momentum=mom, affine=True, track_running_stats=True)
+        self.conv5 = nn.Conv2d(set1, set2, 3, padding=1)
+        self.bn5 = nn.BatchNorm2d(num_features=set2, eps=1e-05, momentum=mom, affine=True, track_running_stats=True)
+        self.conv6 = nn.Conv2d(set2, set2, 3)
+        self.bn6 = nn.BatchNorm2d(num_features=set2, eps=1e-05, momentum=mom, affine=True, track_running_stats=True)
+        self.conv7 = nn.Conv2d(set2, set2, 1)
+        self.bn7 = nn.BatchNorm2d(num_features=set2, eps=1e-05, momentum=mom, affine=True, track_running_stats=True)
+        self.pool2 = nn.MaxPool2d(2, 2)
+
+        self.conv8 = nn.Conv2d(set2, set2, 3)
+        self.bn8 = nn.BatchNorm2d(num_features=set2, eps=1e-05, momentum=mom, affine=True, track_running_stats=True)
+        self.conv9 = nn.Conv2d(set2, set2, 3)
+        self.bn9 = nn.BatchNorm2d(num_features=set2, eps=1e-05, momentum=mom, affine=True, track_running_stats=True)
+        self.conv10 = nn.Conv2d(set2, out, 3)
+        self.bn10 = nn.BatchNorm2d(num_features=out, eps=1e-05, momentum=mom, affine=True, track_running_stats=True)
+
+        self.gap = nn.AvgPool2d(kernel_size=[avg,avg], stride=[avg,avg], padding=0, ceil_mode=False, count_include_pad=False)
+        self.conv11 = nn.Conv2d(out, out, 1)
+
+
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+        x = self.pool1(self.bn3(F.relu(self.conv3(self.bn2(F.relu(self.conv2(self.bn1(F.relu(self.conv1(x))))))))))
+        x = self.pool2(self.bn7(F.relu(self.conv7(self.bn6(F.relu(self.conv6(self.bn5(F.relu(self.conv5(self.bn4(F.relu(self.conv4(x)))))))))))))
+        x = self.bn10(F.relu(self.conv10(self.bn9(F.relu(self.conv9((self.bn8(F.relu(self.conv8(x))))))))))
+        #print(x.shape)
+        x = self.conv11(x)
+        #print(x.shape)
+        #x = self.gap(x)
+        #print(x.shape)
+        x = x.view(-1, 10) # Raja ToDo Try printing shape here
+        return F.log_softmax(x)
+
+
 ## For S8 of EraV2 - Group Normalization
-
-
 
 class Net_GN_S8(nn.Module):
     def __init__(self):

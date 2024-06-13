c19bb9162a7d665ba98f3c88e0cc29442aac106f
raja <rajap.nitc@gmail.com>
2024-06-13 22:03:52 +0530

    move wget out of py file

--- S19/s19_cpu_gradio.py
+++ S19/s19_cpu_gradio.py
@@ -1,6 +1,4 @@
 def GPT():
-  # We always start with a dataset to train on. Let's download the tiny shakespeare dataset
-  !wget https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt
 
 # read it in to inspect it
   with open('input.txt', 'r', encoding='utf-8') as f:

22032dc15521210ac50b5408840c7d0ea4086027
raja <rajap.nitc@gmail.com>
2024-06-14 21:53:05 +0530

    open of input.txt added in py

--- S19/s19_cpu_gradio_2.py
+++ S19/s19_cpu_gradio_2.py
@@ -34,7 +34,8 @@ def GPT(context, max_new_tokens=100):
   
   torch.manual_seed(1337)
   
-  
+  with open('input.txt', 'r', encoding='utf-8') as f:
+      text = f.read()
   # here are all the unique characters that occur in this text
   chars = sorted(list(set(text)))
   vocab_size = len(chars)
@@ -43,36 +44,6 @@ def GPT(context, max_new_tokens=100):
   itos = { i:ch for i,ch in enumerate(chars) }
   encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
   decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
-  
-  # Train and test splits
-  data = torch.tensor(encode(text), dtype=torch.long)
-  n = int(0.9*len(data)) # first 90% will be train, rest val
-  train_data = data[:n]
-  val_data = data[n:]
-  
-  # data loading
-  def get_batch(split):
-      # generate a small batch of data of inputs x and targets y
-      data = train_data if split == 'train' else val_data
-      ix = torch.randint(len(data) - block_size, (batch_size,))
-      x = torch.stack([data[i:i+block_size] for i in ix])
-      y = torch.stack([data[i+1:i+block_size+1] for i in ix])
-      x, y = x.to(device), y.to(device)
-      return x, y
-  
-  @torch.no_grad()
-  def estimate_loss():
-      out = {}
-      model.eval()
-      for split in ['train', 'val']:
-          losses = torch.zeros(eval_iters)
-          for k in range(eval_iters):
-              X, Y = get_batch(split)
-              logits, loss = model(X, Y)
-              losses[k] = loss.item()
-          out[split] = losses.mean()
-      model.train()
-      return out
   
   class Head(nn.Module):
       """ one head of self-attention """
